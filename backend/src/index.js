26d97387348431ffda02708f698f46c8339b1af6
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-11-17 18:15:52 +0530

    day2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/db.js';
+
 
 dotenv.config();
 const app = express();
@@ -10,4 +12,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
+    connectDB();
 })
